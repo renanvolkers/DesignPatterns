35ba35a8c886f00d3b823159bc46e5a08688c6c0
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-23 00:39:46 -0300

    Class Concreet with metod Open conection for sqlserve.

--- AbstractFactory/AbstractFactory/Common/Concrete/SqlConection.cs
+++ AbstractFactory/AbstractFactory/Common/Concrete/SqlConection.cs
@@ -1,11 +1,12 @@
-﻿namespace AbstractFactory.Common
+﻿using AbstractFactory.Common.Interface;
+namespace AbstractFactory.Common.Concrete
 {
     /* Concret SqlConnection */
     public class SqlConnection : DbConnection
     {
         public override void Open()
         {
-            Console.WriteLine(&amp; amp; amp; amp; quot; Método Open de SQL Connection foi chamado...&amp; amp; amp; amp; quot;);
+            Console.WriteLine("amp amp; amp; amp; quot; Método Open de SQL Connection foi chamado...&amp; amp; amp; amp; quot;");
         }
     }
 }
