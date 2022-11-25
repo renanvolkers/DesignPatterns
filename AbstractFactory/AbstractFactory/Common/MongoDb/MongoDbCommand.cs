b67ee539eefb907b1b4387a3836ba4b2503f0399
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-25 19:53:05 -0300

    Execute Command MogoDB return result

---
+++ AbstractFactory/AbstractFactory/Common/MongoDb/MongoDbCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace AbstractFactory.Common.SqlServe
+{
+    public class MongoDbCommand : DbCommand
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("&amp; amp; amp; amp; quot; Executando o comando do sql do Mongo.....& amp; amp; amp; amp; quot;");
+        }
+    }
+}
+
+
