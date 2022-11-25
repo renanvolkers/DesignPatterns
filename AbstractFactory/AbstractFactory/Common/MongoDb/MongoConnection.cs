783c783c86bfd74319662cd2e23d9512d174d65a
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-25 19:52:07 -0300

    Execute Command MogoDB filter

---
+++ AbstractFactory/AbstractFactory/Common/MongoDb/MongoConnection.cs
@@ -0,0 +1,11 @@
+﻿using AbstractFactory.Common.Interface;
+namespace AbstractFactory.Common.MongoDb
+{
+    public class MongoConnection : DbConnection
+    {
+        public override void Open()
+        {
+            Console.WriteLine("amp amp; amp; amp; quot; Método Open de SQL Connection foi chamado...&amp; amp; amp; amp; quot;");
+        }
+    }
+}
