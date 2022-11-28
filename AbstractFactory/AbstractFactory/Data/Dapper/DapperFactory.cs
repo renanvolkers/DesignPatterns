6aff44b8f343fbbf06170ef34f45f64ef5de4cda
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-27 22:40:39 -0300

    Create Dapper Factory

---
+++ AbstractFactory/AbstractFactory/Data/Dapper/DapperFactory.cs
@@ -0,0 +1,17 @@
+﻿using AbstractFactory.Data.Interface;
+
+namespace AbstractFactory.Data.Dapper
+{
+    public class DapperFactory : DbFactory
+    {
+        public override DbCommand CreateCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DbConnection CreateConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
