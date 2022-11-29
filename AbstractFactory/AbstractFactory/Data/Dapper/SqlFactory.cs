bd88e6e98f695557260ef7ebfa2a83d5b62a5e04
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-28 23:59:30 -0300

    Class sql factory

---
+++ AbstractFactory/AbstractFactory/Data/Dapper/SqlFactory.cs
@@ -0,0 +1,19 @@
+﻿using AbstractFactory.Data.Interface;
+
+namespace AbstractFactory.Data.Dapper
+{
+    public abstract class SqlFactory : DbFactory
+    {
+        //    //Concrete Factory
+        public override DbConnection CreateConnection()
+        {
+            /* The class here got encapsul */
+            return new SqlConnection();
+        }
+
+        public override DbCommand CreateCommand()
+        {
+            return  new SqlCommand();
+        }
+    }
+}
