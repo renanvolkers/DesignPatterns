5b6bb9c648b704a6da49b7e7d1938af640f94710
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-29 22:37:39 -0300

    Class Croncret Chicago Cheese Pizza Product

---
+++ AbstractFactory/AbstractFactory/StorePizza/Chicago/ChicagoStyleCheesePizza.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace AbstractFactory.StorePizza
+{
+    public class ChicagoStyleCheesePizza : Pizza
+    {
+        public ChicagoStyleCheesePizza()
+        {
+            Name = "Chicago Style Deep Dish Cheese Pizza";
+            Dough = "Extra Thick Crust Dough";
+            Sauce = "Plum tomato Sauce";
+
+            Toppings.Add("Shredded Mozzarella Cheese");
+        }
+
+        public override void Cut()
+        {
+            var sb = new StringBuilder(StatusDescription);
+            sb.AppendLine("Cutting the pizza into square slices");
+            StatusDescription = sb.ToString();
+        }
+    }
+}
