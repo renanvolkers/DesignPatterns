036aa81562ecc0a62bbd4c67b47e5887a9ba79c4
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-30 14:39:21 -0300

    Include Enum city

---
+++ AbstractFactory/AbstractFactory/StorePizza/Type.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace AbstractFactory.StorePizza
+{
+   // [Flags]
+    public enum TypePizza
+    {
+        Cheese = 0,
+        Viggie = 1,
+        Clam = 2,
+        Pepperoni = 4,
+
+    }
+    //[JsonConverter(typeof(City))]
+    public enum City
+    {
+        NY = 0,
+        Chicago = 1,
+        California = 2,
+
+    }
+
+    [JsonConverter(typeof(string))]
+    public enum MessageDestinationType
+    {
+        None = 0,
+        [EnumMember(Value = "SMS")]
+        SMS = 1,
+        [EnumMember(Value = "Email")]
+        Email = 2,
+        PushNotification = 3,
+        UXP = 4,
+        IntermediaryApp = 5,
+        YouthApp = 6
+    }
+}
