60300d8b3b1e70351656cda8c34de6d5c88e83eb
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-29 22:40:35 -0300

    Create Pizza and the subclass is fully responsable for which concrete pizza.

---
+++ AbstractFactory/AbstractFactory/StorePizza/NY/NYPizzaStore.cs
@@ -0,0 +1,26 @@
+﻿namespace AbstractFactory.StorePizza
+{
+    public class NYPizzaStore: PizzaStore
+    {
+       protected override  Pizza CreatePizza(TypePizza type)
+        {
+            if (type.Equals(TypePizza.Cheese))
+            {
+                return new NYStyleCheesePizza();
+            }
+            else if (type.Equals(TypePizza.Viggie))
+            {
+                return new NYStyleVeggiePizza();
+            }
+            else if (type.Equals(TypePizza.Clam))
+            {
+                return new NYStyleClamPizza();
+            }
+            else if (type.Equals(TypePizza.Pepperoni))
+            {
+                return new NYStylePepperoniPizza();
+            }
+            else return null;
+        }
+    }   
+}
