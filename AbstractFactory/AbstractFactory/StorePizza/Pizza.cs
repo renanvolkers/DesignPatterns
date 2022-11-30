7b0adbb0fa8fd55874a91c22457befbc167cd18c
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-29 22:46:29 -0300

    This is the product of the factory pizza. we've defined  pizza as an abstract classe withi some helpful implemention that can be overridden.

---
+++ AbstractFactory/AbstractFactory/StorePizza/Pizza.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace AbstractFactory.StorePizza
+{
+    public abstract class Pizza
+    {
+        public string? Name { get; set; }
+        public string? Dough { get; set; }
+        public string? Sauce { get; set; }
+        public string? StatusDescription { get; set; }
+
+        public List<string> Toppings { get; set; } = new List<string>();
+
+        public void Prepare()
+        {
+            var sb = new StringBuilder(StatusDescription);
+
+            sb.AppendLine("Preparing " + Name);
+            sb.AppendLine("Tossing dough..");
+            sb.AppendLine("Adding sauce..");
+            sb.AppendLine("Adding Topping");
+
+            Toppings.ForEach(topping => sb.AppendLine("  " + topping));
+            StatusDescription = sb.ToString();
+        }
+
+        public void Bake()
+        {
+            var sb = new StringBuilder(StatusDescription);
+            sb.AppendLine("Bake for 25 minutes at 350");
+            StatusDescription = sb.ToString();
+        }
+        public void Box() {
+            var sb = new StringBuilder(StatusDescription);
+            sb.AppendLine("Place pizza in official PizzaStore box");
+            StatusDescription = sb.ToString();
+        }
+        public virtual void Cut() {
+            var sb = new StringBuilder(StatusDescription);
+            sb.AppendLine("Cutting the pizza into diagonal slices");
+            StatusDescription = sb.ToString();
+        }
+
+    }
+}
