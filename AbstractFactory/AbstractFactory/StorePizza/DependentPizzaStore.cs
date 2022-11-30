dcb5694a2f24a7910bf0a80024384e845e963c86
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-30 18:45:11 -0300

    Class responsable for choose the place

--- AbstractFactory/AbstractFactory/StorePizza/DependentPizzaStore.cs
+++ AbstractFactory/AbstractFactory/StorePizza/DependentPizzaStore.cs
@@ -2,5 +2,28 @@
 {
     public class DependentPizzaStore
     {
+        public Pizza CreatePizza(City city, TypePizza typePizza)
+        {
+            Pizza pizza = null;
+
+            if (City.NY.Equals(city))
+            {
+                var NYStore = new NYPizzaStore();
+                pizza = NYStore.OrderPizza(typePizza);
+                Console.WriteLine(pizza.StatusDescription);
+
+            }
+            else if (City.Chicago.Equals(city))
+            {
+                var ChicagoStore = new ChicagoPizzaStore();
+                pizza = ChicagoStore.OrderPizza(typePizza);
+                Console.WriteLine(pizza.StatusDescription);
+
+            }
+
+            return pizza;
+
+
+        }
     }
 }
