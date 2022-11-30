b3f99d35818698032e5ff301d2a32775a8adc673
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-29 22:35:58 -0300

    Enum with type the pizzas

---
+++ AbstractFactory/AbstractFactory/StorePizza/TypePizza.cs
@@ -0,0 +1,12 @@
+﻿namespace AbstractFactory.StorePizza
+{
+    [Flags]
+    public enum TypePizza
+    {
+        Cheese = 0,
+        Viggie = 1,
+        Clam = 2,
+        Pepperoni = 4,
+
+    }
+}
