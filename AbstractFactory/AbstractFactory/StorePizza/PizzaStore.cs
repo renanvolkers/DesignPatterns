8e4c377143feb3e9017fb09d243fa88764b3fe06
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-29 22:47:52 -0300

    Now we’ve got a store waiting for subclasses; we’re going to have a subclass for each regional type (NYPizzaStore, ChicagoPizzaStore, CaliforniaPizzaStore)

---
+++ AbstractFactory/AbstractFactory/StorePizza/PizzaStore.cs
@@ -0,0 +1,22 @@
+﻿namespace AbstractFactory.StorePizza
+{
+    public abstract class PizzaStore
+    {
+       
+
+        public Pizza OrderPizza(TypePizza type)
+        {
+            Pizza? pizza;
+            pizza = CreatePizza(type);
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+            
+            return pizza;
+        }
+
+       protected  abstract Pizza CreatePizza(TypePizza type);
+    }   
+}
