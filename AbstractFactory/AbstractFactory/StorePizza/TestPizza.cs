cd7cc89e95a5a19d07d03e7afbf80aa47df8a151
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-11-29 22:43:50 -0300

    Main Execute Pizza for test

---
+++ AbstractFactory/AbstractFactory/StorePizza/TestPizza.cs
@@ -0,0 +1,24 @@
+﻿namespace AbstractFactory.StorePizza
+{
+    public class TestPizza
+    {
+        public TestPizza()
+        {
+
+        }
+
+        public  string Execute(TypePizza typePizza)
+        {
+            PizzaStore NYStore = new NYPizzaStore();
+            PizzaStore ChicagoStore = new ChicagoPizzaStore();
+
+            Pizza pizza = NYStore.OrderPizza(typePizza);
+            System.Console.WriteLine(pizza.StatusDescription);
+
+             pizza = ChicagoStore.OrderPizza(typePizza);
+            System.Console.WriteLine(pizza.StatusDescription);
+
+            return pizza?.StatusDescription;
+        }
+    }
+}
