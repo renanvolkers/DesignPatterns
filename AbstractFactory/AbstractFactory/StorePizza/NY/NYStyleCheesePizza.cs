c5211806b54d30a85ecd6ba5432c2149d11b612d
Renan <renanvolkers@hotmail.com>
2022-12-01 23:07:19 -0300

    Update NYStyleCheesePizza.cs
    Size NY style

--- AbstractFactory/AbstractFactory/StorePizza/NY/NYStyleCheesePizza.cs
+++ AbstractFactory/AbstractFactory/StorePizza/NY/NYStyleCheesePizza.cs
@@ -7,6 +7,7 @@
             Name = "NY Style Sauce and Cheese Pizza";
             Dough = "Thin Crust Dough";
             Sauce = "Marinara Sauce";
+            Size =5;
 
             Toppings.Add("Grated Reggiano Cheese");
         }
