ac1ba4db836abcb3ba0ed23a9cd351d06cf57805
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-02 23:58:51 -0300

    Responsable centers Endpoints Pizza

---
+++ AbstractFactory/AbstractFactory/Example_1/EndPoints/PizzaEndPoints.cs
@@ -0,0 +1,27 @@
+﻿using AbstractFactory.Example_1.Domain.Entities;
+using AbstractFactory.Example_1.Domain.Interface;
+using AbstractFactory.Example_1.Domain.ValueObj;
+
+namespace AbstractFactory.Example_1.EndPoints
+{
+    public static class  PizzaEndPoints
+    {
+        public static void MapPizzaEndPoints(this WebApplication app)
+        {
+            app.MapGet("/StorePizza/{city}/{typePizza}", Get)
+                .ProducesValidationProblem()
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithName("StorePizza")
+                .WithOpenApi() ;
+        }
+
+        public static IResult Get(City city, TypePizza typePizza)
+        {
+            DependentPizzaStore main = new DependentPizzaStore();
+
+            var pizza = main.CreatePizza(city, typePizza);
+            return pizza is IPizza ? Results.Ok(pizza.StatusDescription)
+                                  : Results.NotFound();
+        }
+    }
+}
