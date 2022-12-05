9a171991c010dc431c4a1f2be14ae2e78cba463a
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-04 23:53:08 -0300

    Generic Validation Error Custom Message

---
+++ AbstractFactory/AbstractFactory/Example_1/Extensions/ValidationErrorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+
+namespace AbstractFactory.Example_1.Extensions
+{
+    public static class ValidationErrorExtensions
+    {
+        public static string GetErros(this List<ValidationFailure> erros)
+        {
+            var errorMessages = "";
+            erros.ForEach(err => errorMessages += err.ErrorMessage + " ");
+            
+            return errorMessages;
+        }
+    }
+}
