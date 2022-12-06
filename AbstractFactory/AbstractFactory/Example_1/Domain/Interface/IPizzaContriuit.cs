83d2085bd23e6d77292359dd2d5ea636f4feff19
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-06 00:31:37 -0300

    Create IPizzaContriuit.cs

---
+++ AbstractFactory/AbstractFactory/Example_1/Domain/Interface/IPizzaContriuit.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace AbstractFactory.Example_1.Domain.Interface
+{
+    public abstract class IPizza
+    {
+        //Product 
+        public string? Name { get; set; }
+        public string? Dough { get; set; }
+        public string? Sauce { get; set; }
+        public int Size { get; set; }
+        public string? StatusDescription { get; set; }
+
+        public List<string> Toppings { get; set; }
+
+        public void Prepare()
+        {
+            var sb = new StringBuilder(StatusDescription);
+
+            sb.AppendLine("Preparing " + Name);
+            sb.AppendLine("Tossing dough..");
+            sb.AppendLine("Adding sauce..");
+            sb.AppendLine("Adding Topping");
+
+            Toppings.ForEach(topping => sb.AppendLine("  " + topping));
+            StatusDescription = sb.ToString();
+        }
+
+        public void Bake()
+        {
+            var sb = new StringBuilder(StatusDescription);
+            sb.AppendLine("Bake for 25 minutes at 350");
+            StatusDescription = sb.ToString();
+        }
+        public void Box()
+        {
+            var sb = new StringBuilder(StatusDescription);
+            sb.AppendLine("Place pizza in official PizzaStore box");
+            StatusDescription = sb.ToString();
+        }
+        public virtual void Cut()
+        {
+            var sb = new StringBuilder(StatusDescription);
+            sb.AppendLine("Cutting the pizza into diagonal slices");
+            StatusDescription = sb.ToString();
+        }
+
+    }
+}
