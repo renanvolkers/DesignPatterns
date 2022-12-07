89058263daee4254e28b15df941c4c3ce31e8315
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-07 00:00:21 -0300

    Construct class NYStyleVeggiePizza

--- AbstractFactory/AbstractFactory/Example_1/Domain/Entities/NYStyleVeggiePizza.cs
+++ AbstractFactory/AbstractFactory/Example_1/Domain/Entities/NYStyleVeggiePizza.cs
@@ -4,5 +4,15 @@ namespace AbstractFactory.Example_1.Domain.Entities
 {//Concrete product 
     public class NYStyleVeggiePizza : IPizza
     {
+        public NYStyleVeggiePizza()
+        {
+            Name = "NY Style Sauce and Veggie Pizza";
+            Dough = "Thin Crust Dough";
+            Sauce = "Marinara Sauce";
+            Size = 5;
+            Toppings = new List<string>();
+
+            Toppings.Add("Grated Veggie Veggie");
+        }
     }
 }
