c959f60b44106ce152a5cf50dfe8975ccc5a5454
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-07 13:04:05 -0300

    New Method get errors in Dictionary

--- AbstractFactory/AbstractFactory/Example_1/Extensions/ValidationErrorExtensions.cs
+++ AbstractFactory/AbstractFactory/Example_1/Extensions/ValidationErrorExtensions.cs
@@ -2,14 +2,22 @@
 
 namespace AbstractFactory.Example_1.Extensions
 {
-    public static class ValidationErrorExtensions
+    public static class ValidationFilterFactory
     {
-        public static string GetErros(this List<ValidationFailure> erros)
+        public static string GetErrors(this List<ValidationFailure> erros)
         {
             var errorMessages = "";
             erros.ForEach(err => errorMessages += err.ErrorMessage + " ");
-            
+
             return errorMessages;
+        }
+
+        public static IDictionary<string, string[]> AsDictionary(this List<ValidationFailure> erros)
+        {
+            var dict = new Dictionary<string, string[]>();
+            dict.Add("Errors", erros.Select(x => x.ToString()).ToArray());
+
+            return dict;
         }
     }
 }
