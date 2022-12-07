630d405283d8d535fa10b0d270abd260ca4e5249
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-07 13:05:09 -0300

    Class Validation Filter use nuget validation for show message.

---
+++ AbstractFactory/AbstractFactory/Example_1/Extensions/ValidationFilter.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace AbstractFactory.Example_1.Extensions
+{
+    public class ValidationFilter<T> : IEndpointFilter where T : class
+    {
+        private readonly IValidator<T> _validator;
+        public ValidationFilter(IValidator<T> validator)
+        {
+            _validator = validator;
+        }
+
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context,
+                      EndpointFilterDelegate next)
+        {
+            var parameter = context.Arguments.SingleOrDefault(p => p.GetType() == typeof(T));
+
+            if (parameter is null) return Results.BadRequest("O parametro é inválido/Type.");
+
+            var result = await _validator.ValidateAsync((T)parameter);
+
+            if (!result.IsValid)
+            {
+                return Results.ValidationProblem(result.Errors.AsDictionary());
+            }
+            // now the actual endpoint execution
+            return await next(context);
+        }
+
+    }
+}
