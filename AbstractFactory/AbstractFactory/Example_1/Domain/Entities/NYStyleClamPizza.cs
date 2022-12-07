df56d43be553e0c5699ebcdf1cfd94e46ce451e1
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-06 23:58:17 -0300

    NYStyleClamPizza Construct Pizza NY

--- AbstractFactory/AbstractFactory/Example_1/Domain/Entities/NYStyleClamPizza.cs
+++ AbstractFactory/AbstractFactory/Example_1/Domain/Entities/NYStyleClamPizza.cs
@@ -5,5 +5,15 @@ namespace AbstractFactory.Example_1.Domain.Entities
     //Concrete product 
     public class NYStyleClamPizza : IPizza
     {
+        public NYStyleClamPizza()
+        {
+            Name = "NY Style Sauce and Clam Pizza";
+            Dough = "Thin Crust Dough";
+            Sauce = "Marinara Sauce";
+            Size = 5;
+            Toppings = new List<string>();
+
+            Toppings.Add("Grated Reggiano Clam");
+        }
     }
 }
