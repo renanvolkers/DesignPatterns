35295451ddc59afaad71d9fb24c4e07d3e58b589
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-06 23:56:00 -0300

    Comment Create MapPost

--- AbstractFactory/AbstractFactory/Example_1/EndPoints/PizzaEndPoints.cs
+++ AbstractFactory/AbstractFactory/Example_1/EndPoints/PizzaEndPoints.cs
@@ -17,16 +17,16 @@ namespace AbstractFactory.Example_1.EndPoints
 
                 .WithOpenApi() ;
 
-            app.MapPost("/StorePizza/{city}/{typePizza}", Create).AddFilter(async (routeHandlerInvocationContext, next) =>
-            {
-                var name = (string)routeHandlerInvocationContext.Parameters[0];
-				var pizza = (IPizza)routeHandlerInvocationContext.Parameters[1];
-                if (name == "Bob")
-                {
-                    return Results.Problem("No Bob's allowed");
-                }
-                return await next(routeHandlerInvocationContext);
-            }).ProducesValidationProblem();
+    //        app.MapPost("/StorePizza/{city}/{typePizza}", Create).AddFilter(async (routeHandlerInvocationContext, next) =>
+    //        {
+    //            var name = (string)routeHandlerInvocationContext.Parameters[0];
+				//var pizza = (IPizza)routeHandlerInvocationContext.Parameters[1];
+    //            if (name == "Bob")
+    //            {
+    //                return Results.Problem("No Bob's allowed");
+    //            }
+    //            return await next(routeHandlerInvocationContext);
+    //        }).ProducesValidationProblem();
         }
 
         public static IResult Get(IValidator<IPizza> validator ,[FromQuery] City city, [FromQuery] TypePizza typePizza)
