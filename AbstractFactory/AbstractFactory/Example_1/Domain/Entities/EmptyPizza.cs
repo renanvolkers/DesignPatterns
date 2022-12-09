fd18e415064f7d51eea46840ca17a8a045e12594
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-08 23:59:24 -0300

    Pizza Empty use in create validation into DI

---
+++ AbstractFactory/AbstractFactory/Example_1/Domain/Entities/EmptyPizza.cs
@@ -0,0 +1,18 @@
+﻿using AbstractFactory.Example_1.Domain.Interface;
+
+namespace AbstractFactory.Example_1.Domain.Entities
+{
+    public class EmptyPizza : IPizza
+    {
+        public EmptyPizza()
+        {
+            Name = "Empty Pizza";
+            Dough = "Empty Pizza";
+            Sauce = "Empty Pizza";
+            Size = 5;
+            Toppings = new List<string>();
+
+            Toppings.Add("Empty");
+        }
+    }
+}
