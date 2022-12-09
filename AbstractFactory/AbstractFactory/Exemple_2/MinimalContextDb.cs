e075de5a5b08aa7d1c625d5da991cb44275f1cf1
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-09 20:22:58 -0300

    Model Entity OnModelCreating

--- AbstractFactory/AbstractFactory/Exemple_2/MinimalContextDb.cs
+++ AbstractFactory/AbstractFactory/Exemple_2/MinimalContextDb.cs
@@ -5,5 +5,9 @@ namespace AbstractFactory.Exemple_2
     public class MinimalContextDb :DbContext
     {
         public MinimalContextDb(DbContextOptions<MinimalContextDb> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+        
+        }
     }
 }
