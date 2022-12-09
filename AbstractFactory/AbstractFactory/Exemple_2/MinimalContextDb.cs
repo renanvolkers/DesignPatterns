b8da5ca4c5829fc1e084bee37f467a027fd901fe
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-09 20:05:48 -0300

    Class DbContext create model

---
+++ AbstractFactory/AbstractFactory/Exemple_2/MinimalContextDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AbstractFactory.Exemple_2
+{
+    public class MinimalContextDb :DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options): base(options) { }
+    }
+}
