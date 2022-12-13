0314195cc371462bdd0f6a5ca68da3f649a7d70a
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-12 23:56:06 -0300

    model is discovered by running a set

--- AbstractFactory/AbstractFactory/Exemple_2/MinimalContextDb.cs
+++ AbstractFactory/AbstractFactory/Exemple_2/MinimalContextDb.cs
@@ -5,6 +5,7 @@ namespace AbstractFactory.Exemple_2
 {
     public class MinimalContextDb :DbContext
     {
+        //The model is discovered by running a set of conventions over the entity classes found in the
         public MinimalContextDb(DbContextOptions<MinimalContextDb> options): base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
