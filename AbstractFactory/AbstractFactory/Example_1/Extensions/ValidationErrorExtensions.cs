02673bc7f585cabf74fbb0bbabbd0ca0121f25fb
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-13 23:30:55 -0300

    New methodo list dictionary

--- AbstractFactory/AbstractFactory/Example_1/Extensions/ValidationErrorExtensions.cs
+++ AbstractFactory/AbstractFactory/Example_1/Extensions/ValidationErrorExtensions.cs
@@ -19,5 +19,14 @@ namespace AbstractFactory.Example_1.Extensions
 
             return dictionary;
         }
+        public static IDictionary<string, string[]> ToDictionary(this ValidationResult validationResult)
+        {
+            return validationResult.Errors
+              .GroupBy(x => x.PropertyName)
+              .ToDictionary(
+                g => g.Key,
+                g => g.Select(x => x.ErrorMessage).ToArray()
+              );
+        }
     }
 }
