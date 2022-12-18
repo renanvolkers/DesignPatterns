f125769e954edae72782b3dd55c55adfee4393cd
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-17 23:59:43 -0300

    Create Procuto factory model

--- AbstractFactory/AbstractFactory/Exemple_2/Factories/AbstracFactory/ProductAbstractFactory.cs
+++ AbstractFactory/AbstractFactory/Exemple_2/Factories/AbstracFactory/ProductAbstractFactory.cs
@@ -1,7 +1,11 @@
-﻿namespace AbstractFactory.Exemple_2.Factories.AbstracFactory
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AbstractFactory.Exemple_2.Factories.AbstracFactory
 {
     //Abstract Factory
     public abstract class ProductAbstractFactory
     {
+       protected abstract void OnModelCreating(ModelBuilder modelBuilder);
     }
+
 }
