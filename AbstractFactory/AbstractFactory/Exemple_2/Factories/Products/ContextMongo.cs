e4214ea9d7ceac7d619a1a7c4a0006afd7a0028c
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-28 23:44:18 -0300

    Concrete Factory Defirente dataBase

---
+++ AbstractFactory/AbstractFactory/Exemple_2/Factories/Products/ContextMongo.cs
@@ -0,0 +1,38 @@
+﻿using AbstractFactory.Exemple_2.Domain;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using System.Linq.Expressions;
+
+namespace AbstractFactory.Exemple_2.Factories.Products
+{
+    public class ContextMongo : DbContext, ContextDataBase
+    {
+        public string? ConnectionString { get; set; }
+        public string? DatabaseName { get; set; }
+        public bool IsSSL { get; set; }
+        private IMongoDatabase? _database { get; set; }
+
+        public void OnConfiguring()
+        {
+            try
+            {
+                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(ConnectionString));
+                if (IsSSL)
+                {
+                    settings.SslSettings = new SslSettings { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 };
+                }
+                var mongoClient = new MongoClient(settings);
+                _database = mongoClient.GetDatabase(DatabaseName);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Não foi possível se conectar com o servidor.", ex);
+            }
+        }
+
+        public IQueryable<Product> Get(Expression<Func<Product, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AbstractFactory/AbstractFactory/Exemple_2/Factories/Products/ContextSqlEE.cs
@@ -0,0 +1,28 @@
+﻿using AbstractFactory.Exemple_2.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AbstractFactory.Exemple_2.Factories.Products
+{
+    public class ContextSqlEE : DbContext,  ContextDataBase
+    {
+        public string ConnectionString { get; set; }
+        public string? DatabaseName { get; set; }
+
+        protected  void  OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConnectionString);
+            }
+        }
+        public DbSet<Product> Product { get; set; }
+
+        public IQueryable<Product> Get(Expression<Func<Product, bool>> predicate) => Product.AsQueryable().Where(predicate);
+
+        public void OnConfiguring()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
