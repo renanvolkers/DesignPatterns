69a0ecf5f6195d1614e664f83b890f6f872bbce5
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-28 23:41:50 -0300

    Context Multiple database

---
+++ AbstractFactory/AbstractFactory/Exemple_2/Domain/ContextDataBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace AbstractFactory.Exemple_2.Domain
+{
+    public interface  ContextDataBase
+    {
+        public void OnConfiguring();
+        public  IQueryable<Product> Get(Expression<Func<Product, bool>> predicate);
+    }
+}
---
+++ AbstractFactory/AbstractFactory/Exemple_2/Domain/IApplicationDB.cs
@@ -0,0 +1,7 @@
+﻿namespace AbstractFactory.Exemple_2.Domain
+{
+    public interface IApplicationDB
+    {
+      public  string ConnectionString { get; set; }
+    }
+}
