13def143674ee13bb10d0492c36ee5b511c86f57
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2022-12-28 23:43:44 -0300

    Pattern Abstract Factory Exemple

---
+++ AbstractFactory/AbstractFactory/Exemple_2/Factories/AbstracFactory/ManagerDataBaseAbstractFactory.cs
@@ -0,0 +1,11 @@
+﻿using AbstractFactory.Exemple_2.Domain;
+
+namespace AbstractFactory.Exemple_2.Factories.AbstracFactory
+{
+    //Abstract Factory
+    public abstract class ManagerDataBaseAbstractFactory
+    {
+       public abstract ContextDataBase CreateDbContext();
+    }
+
+}
