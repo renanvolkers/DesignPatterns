f26566216b1010b4b64e35172861738103db8366
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-06 00:00:04 -0300

    ApplicationDB factory

---
+++ AbstractFactory/AbstractFactory/Exemple_2/Factories/ApplicationDB.cs
@@ -0,0 +1,29 @@
+﻿using AbstractFactory.Exemple_2.Domain;
+using AbstractFactory.Exemple_2.Factories.AbstracFactory;
+
+namespace AbstractFactory.Exemple_2.Factories
+{
+    public class ApplicationDB : IApplicationDB
+    {
+        private ManagerDataBaseAbstractFactory factory;
+        private ContextDataBase contextDB;
+        public string ConnectionString { get; set; }
+
+
+        public ApplicationDB(string options)
+        {
+            if (options == "MongoDB")
+            {
+                factory = new ContextMongoDBFactory();
+                contextDB = factory.CreateDbContext();
+            }
+            else
+            {
+                factory = new ContextSqlDBFactory();
+                contextDB = factory.CreateDbContext();
+            }
+
+        }
+
+    }
+}
