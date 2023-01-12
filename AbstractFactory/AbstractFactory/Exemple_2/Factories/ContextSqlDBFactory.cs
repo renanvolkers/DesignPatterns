f6ec469c1c01f71a4ac3e957f319b6d9b6f14ce7
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-11 23:12:53 -0300

    Context Acess multiple data base

---
+++ AbstractFactory/AbstractFactory/Exemple_2/Factories/ContextSqlDBFactory.cs
@@ -0,0 +1,13 @@
+﻿using AbstractFactory.Exemple_2.Domain;
+using AbstractFactory.Exemple_2.Factories.AbstracFactory;
+
+namespace AbstractFactory.Exemple_2.Factories
+{
+    public class ContextSqlDBFactory : ManagerDataBaseAbstractFactory
+    {
+        public override ContextDataBase CreateDbContext()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
