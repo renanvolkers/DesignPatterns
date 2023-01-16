9061277b3e8d275eea3a617bb47063d73601d183
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-16 15:32:09 -0300

    Dont't show propriely in request.

--- AbstractFactory/AbstractFactory/Exemple_2/Domain/Product.cs
+++ AbstractFactory/AbstractFactory/Exemple_2/Domain/Product.cs
@@ -1,7 +1,10 @@
-﻿namespace AbstractFactory.Exemple_2.Domain
+﻿using System.Text.Json.Serialization;
+
+namespace AbstractFactory.Exemple_2.Domain
 {
     public class Product
     {
+        [JsonIgnore]
         public int Id       { get; set; }
         public string? Name {get;set;}
     }
