45c652cad5ce762646e380062b66ac52c85172eb
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-18 23:55:50 -0300

    Add class custom GetErros

---
+++ FactoryMethod/FactoryMethod/Translator/Extensions/ValidationErrorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace FactoryMethod.Translator.Extensions
+{
+    public static class ValidationFilterFactory
+    {
+        public static string GetErrors(this List<ValidationFailure> erros)
+        {
+            var errorMessages = "";
+            erros.ForEach(err => errorMessages += err.ErrorMessage + " ");
+
+            return errorMessages;
+        }
+
+        public static IDictionary<string, string[]> AsDictionary(this List<ValidationFailure> erros)
+        {
+
+            var dictionary = new Dictionary<string, string[]>();
+            dictionary.Add("Errors", erros.Select(x => x.ToString()).ToArray());
+
+            return dictionary;
+        }
+
+    }
+}
