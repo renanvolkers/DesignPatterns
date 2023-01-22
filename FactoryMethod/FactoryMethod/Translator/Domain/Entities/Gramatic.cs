8e5b1fdf914e9dcaf10660ee31ce887142a2e912
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-21 23:52:07 -0300

    Object with Rules gramatic

---
+++ FactoryMethod/FactoryMethod/Translator/Domain/Entities/Gramatic.cs
@@ -0,0 +1,10 @@
+﻿namespace FactoryMethod.Translator.Domain.Entities
+{
+    public class Gramatic
+    {
+        public int Id { get; set; }
+        public List<PromonounsObject>? PromonounsObject { get; set; }
+        public List<PossessiveAdjective>? PossessiveAdjective { get; set; }
+
+    }
+}
