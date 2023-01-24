a378c99f747a874ce7dcf0e059010fc0bb866edc
Renan <renanvolkers@hotmail.com>
2023-01-23 23:17:43 -0300

    Update English.cs
    Interface method grammar

--- FactoryMethod/FactoryMethod/Translator/Domain/Entities/English.cs
+++ FactoryMethod/FactoryMethod/Translator/Domain/Entities/English.cs
@@ -1,6 +1,6 @@
 ﻿namespace FactoryMethod.Translator.Domain.Entities
 {
-    public class English
+    public class English: ILanguage
     {
         public int Id { get; set; }
         public PossessiveAdjective PossessiveAdjective { get; set; }
