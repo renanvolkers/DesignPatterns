45759259213334ca9bd790d3e848d5a69e0aeee5
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-24 23:40:55 -0300

    Object is The function of the word is to represent parts of human thought,

---
+++ FactoryMethod/FactoryMethod/Translator/Domain/Entities/Word.cs
@@ -0,0 +1,9 @@
+﻿namespace FactoryMethod.Translator.Domain.Entities
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public char Type { get; set; }
+    }
+}
