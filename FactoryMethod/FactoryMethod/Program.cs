4d6fdb4cd7518c62e6e9043a47d85ba8635414a1
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-25 23:11:18 -0300

    Call EndPoint Map in Start application.

--- FactoryMethod/FactoryMethod/Program.cs
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -1,3 +1,5 @@
+using AbstractFactory.Example_1.EndPoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,7 +18,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPizzaEndPoints();
+app.MapLanguageEndPoints();
 
 app.Run();
 
