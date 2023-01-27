0892b96896f907bbcd17fcdc9124811f394073f0
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-26 23:45:50 -0300

    A MinimalContextDb instance represents a session with the database and can be used to query and save

--- FactoryMethod/FactoryMethod/Program.cs
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -1,15 +1,46 @@
 using AbstractFactory.Example_1.EndPoints;
+using FactoryMethod;
+using FactoryMethod.Translator.Domain.Entities;
+using FactoryMethod.Translator.Validators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers().AddJsonOptions(options =>
+           options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
+
+
+
+
+
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSwaggerGen(op =>
+{
+    op.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Delivery Pizza",
+        Version = "version",
+        Description = "The Goal project is show the use pattern Abstract Factory",
+        Contact = new OpenApiContact
+        {
+            Name = "Renan Alpoim Volkers",
+            Email = "renanvolkers@hotmail.com",
+            Url = new Uri("https://www.linkedin.com/in/renanvolkers/"),
+
+        }
+    });
+
+});
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,7 +48,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.MapLanguageEndPoints();
 
 app.Run();
