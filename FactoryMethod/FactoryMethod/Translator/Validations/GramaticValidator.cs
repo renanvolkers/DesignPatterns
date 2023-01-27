22140b938502526da2dcce56da45c71e47ecaf3d
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-26 23:43:40 -0300

    Validation Gramatic The type of the object being validated

---
+++ FactoryMethod/FactoryMethod/Translator/Validations/GramaticValidator.cs
@@ -0,0 +1,16 @@
+﻿using FactoryMethod.Translator.Domain.Entities;
+using FluentValidation;
+
+namespace FactoryMethod.Translator.Validators
+{
+    public class GramaticValidator<T> : AbstractValidator<Gramatic>
+    {
+        public GramaticValidator()
+        {
+            RuleFor(o=> o.PossessiveAdjective).NotEmpty().NotNull();
+            RuleFor(o => o.PromonounsObject).NotNull();
+
+        }
+    }
+
+}
