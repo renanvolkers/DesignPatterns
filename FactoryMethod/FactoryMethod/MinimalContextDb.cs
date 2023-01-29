18e8076e6f8385c4bf265992d36a922cac70e045
Renan <renanvolkers@hotmail.com>
2023-01-28 23:12:56 -0300

    Update MinimalContextDb.cs
    Remove build model

--- FactoryMethod/FactoryMethod/MinimalContextDb.cs
+++ FactoryMethod/FactoryMethod/MinimalContextDb.cs
@@ -14,54 +14,7 @@ namespace FactoryMethod
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
-            modelBuilder.Entity<PossessiveAdjective>()
-                .HasKey(p => p.Id);
-
-
-
-            modelBuilder.Entity<PossessiveAdjective>()
-                 .Property(p => p.WordId)
-                 .IsRequired();
-
-            modelBuilder.Entity<PossessiveAdjective>()
-                .ToTable("PossessiveAdjective");
-
-            modelBuilder.Entity<Word>()
-                .HasKey(p => p.Id);
-
-
-
-            modelBuilder.Entity<Word>()
-                 .Property(p => p.Name)
-                 .IsRequired();
-
-            modelBuilder.Entity<Word>()
-                 .Property(p => p.Type)
-                 .IsRequired();
-
-
-            modelBuilder.Entity<Word>()
-                .ToTable("Word");
-
-
-
-            modelBuilder.Entity<Word>()
-                .HasKey(p => p.Id);
-
-
-
-            modelBuilder.Entity<PromonounsObject>()
-                 .Property(p => p.Id)
-                 .IsRequired();
-
-            modelBuilder.Entity<PromonounsObject>()
-                 .Property(p => p.WordId)
-                 .IsRequired();
-
-
-            modelBuilder.Entity<PromonounsObject>()
-                .ToTable("PromonounsObject");
-
+      
 
             base.OnModelCreating(modelBuilder);
 
