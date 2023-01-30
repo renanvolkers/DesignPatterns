bc9d3e6f0fc473bbf1afb8481ab1fa1d093ca082
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-30 20:17:43 -0300

    declares the factory method, which returns an object of type Product.

---
+++ FactoryMethod/FactoryMethod/ServiceMessage/Domain/Interface/Channel.cs
@@ -0,0 +1,8 @@
+﻿namespace FactoryMethod.ServiceMessage.Domain.Interface
+{
+    public abstract class Channel
+    {
+       public abstract string? Text { get; }
+       public abstract string SendMessage();
+    }
+}
