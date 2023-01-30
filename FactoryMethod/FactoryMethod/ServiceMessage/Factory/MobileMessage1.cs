2f7d6c7eabb5c2d24536dd826412b6d4a39bc3d0
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-30 20:19:35 -0300

    overrides the factory method to return an instance of a ConcreteProduct.

---
+++ FactoryMethod/FactoryMethod/ServiceMessage/Factory/MobileMessage1.cs
@@ -0,0 +1,16 @@
+﻿using FactoryMethod.ServiceMessage.Domain.Entities;
+using FactoryMethod.ServiceMessage.Domain.Interface;
+
+namespace FactoryMethod.ServiceMessage.Factory
+{
+    // Criadores concretos
+    public class MobileMessage : CreatorMessage
+    {
+        public override void CreateChannel(string text)
+        {
+            this.Channels.Add(new ChatGPT(text));
+            this.Channels.Add(new Whatsapp(text));
+            this.Channels.Add(new Discord(text));
+        }
+    }
+}
---
+++ FactoryMethod/FactoryMethod/ServiceMessage/Factory/WebMessage.cs
@@ -0,0 +1,14 @@
+﻿using FactoryMethod.ServiceMessage.Domain.Entities;
+
+namespace FactoryMethod.ServiceMessage.Factory
+{
+    // Creates concretos
+    public class WebMessage : CreatorMessage
+    {
+        public override void CreateChannel(string text)
+        {
+            Channels.Add(new Email(text));
+        }
+
+    }
+}
