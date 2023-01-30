bc61291ae95000b4c57d72f7f017ef2ab571693d
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-30 20:20:09 -0300

    declares the factory method, which returns an object of type Product.

---
+++ FactoryMethod/FactoryMethod/ServiceMessage/Factory/CreatorMessage.cs
@@ -0,0 +1,23 @@
+﻿using FactoryMethod.ServiceMessage.Domain.Interface;
+
+namespace FactoryMethod.ServiceMessage.Factory
+{
+    public abstract class CreatorMessage
+    {
+        public List<Channel> Channels { get; set; } = new List<Channel> { };
+
+        public CreatorMessage()
+        {
+        }
+        public abstract void CreateChannel(string text);
+        public List<string> SendMessage()
+        {
+            List<string> listMessage = new List<string>();
+            this.Channels.ForEach(x => {
+                listMessage.Add(x.SendMessage());
+            });
+
+            return listMessage;
+        }
+    }
+}
