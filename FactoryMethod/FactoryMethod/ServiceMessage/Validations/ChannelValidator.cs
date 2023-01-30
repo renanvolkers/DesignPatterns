eaf57579693cb8bfa011f7ce424a01b2098e3432
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-01-30 20:20:51 -0300

    The type of the object being validated

---
+++ FactoryMethod/FactoryMethod/ServiceMessage/Validations/ChannelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FactoryMethod.ServiceMessage.Domain.Interface;
+using FluentValidation;
+
+namespace FactoryMethod.ServiceMessage.Validators
+{
+    public class ChannelValidator<T> : AbstractValidator<Channel>
+    {
+        public ChannelValidator()
+        {
+            RuleFor(o=> o.Text).NotEmpty().NotNull();
+        }
+    }
+
+}
