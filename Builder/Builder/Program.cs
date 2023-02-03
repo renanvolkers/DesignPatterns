2f5e24bdb6970f68b9074a5ae95beb8a8a21bb64
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-02 23:10:28 -0300

    Configuration swagger

--- Builder/Builder/Program.cs
+++ Builder/Builder/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,40 +7,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddSwaggerGen(op =>
+{
+    op.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "",
+        Version = "version",
+        Description = "The Goal project is show the use pattern Builder",
+        Contact = new OpenApiContact
+        {
+            Name = "Renan Alpoim Volkers",
+            Email = "renanvolkers@hotmail.com",
+            Url = new Uri("https://www.linkedin.com/in/renanvolkers/"),
 
-// Configure the HTTP request pipeline.
+        }
+    });
+
+});
+var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
