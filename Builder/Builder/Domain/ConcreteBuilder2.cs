54754948ca0b3b46766ff04079320a3925a15e64
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-06 23:44:06 -0300

    ConcreteBuild2 with ihnetinner builder

---
+++ Builder/Builder/Domain/ConcreteBuilder2.cs
@@ -0,0 +1,20 @@
+﻿namespace Builder.Domain
+{
+    public class ConcreteBuilder2 : Builder
+    {
+        public void BuildStepA()
+        {
+            throw new NotImplementedException("BuildStep A condition");
+        }
+
+        public void BuildStepB()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
