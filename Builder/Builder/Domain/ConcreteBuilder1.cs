5214019b0f4ac48ecb3745c43f64f3360d11a1a7
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-06 23:44:33 -0300

    Add ConcreteBuilder1

---
+++ Builder/Builder/Domain/ConcreteBuilder1.cs
@@ -0,0 +1,20 @@
+﻿namespace Builder.Domain
+{
+    public class ConcreteBuilder1 : Builder
+    {
+        public void BuildStepA()
+        {
+            throw new NotImplementedException("Stop Play");
+        }
+
+        public void BuildStepB()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
