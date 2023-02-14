98e73b7097cd5994eb7087ac5f228a513840306c
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-14 11:11:49 -0300

    Add ValidationFilterFactory filter parametrs

---
+++ Builder/Builder/Extensions/ValidationErrorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.Results;
+
+namespace Builder.Extensions
+{
+    public static class ValidationFilterFactory
+    {
+        public static string GetErrors(this List<ValidationFailure> erros)
+        {
+            var errorMessages = "";
+            erros.ForEach(err => errorMessages += err.ErrorMessage + " ");
+
+            return errorMessages;
+        }
+
+        public static IDictionary<string, string[]> AsDictionary(this List<ValidationFailure> erros)
+        {
+
+            var dictionary = new Dictionary<string, string[]>();
+            dictionary.Add("Errors", erros.Select(x => x.ToString()).ToArray());
+
+            return dictionary;
+        }
+
+    }
+}
