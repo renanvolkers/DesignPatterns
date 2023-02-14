2983a388489ab344749e5920b966807a341e38df
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-14 11:35:11 -0300

    Add Interface Build Steps

---
+++ Builder/Builder/Domain/IBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace Builder.Domain
+{
+    public interface IBuilder
+    {
+        public void Reset();
+        public void ConfigAudio();
+        public void Name(string name);
+        public void Video();
+        public void ConfigWireless();
+        public void Raw();
+        public void USB();
+        public void Monitor(string pixel);
+        public void Processor(int fast);
+        public void Configuration();
+        public void MotherBoard();
+        public string Info();
+
+
+
+
+    }
+}
