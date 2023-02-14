07e684de5653f3c24ef2ebaf7a110fcbb16188e0
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-13 23:55:38 -0300

    Interface builder specific methods

--- Builder/Builder/Domain/Builder.cs
+++ Builder/Builder/Domain/Builder.cs
@@ -3,8 +3,14 @@
     public interface Builder
     {
         public void Reset();
-        public void BuildStepA();
-        public void BuildStepB();
+        public void SetAudio();
+        public void SetVideo();
+        public void SetWirelless();
+        public void SetRaw();
+        public void SetUSB();
+        public void SetMonitor();
+        public void SetProcessor();
+        public void SetMotherBoard();
 
     }
 }
