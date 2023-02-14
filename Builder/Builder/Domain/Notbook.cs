364ec957dc50fbf6f779ba447d77f8de803d2a62
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-14 11:34:28 -0300

    Add Product Complex build

---
+++ Builder/Builder/Domain/Notbook.cs
@@ -0,0 +1,18 @@
+﻿namespace Builder.Domain
+{
+    public class Notbook
+    {
+        public string? Name { get; set; }
+        public string? Company { get; set; }
+        public string? Raw { get; set; }
+        public string? HdSsd { get; set; }
+        public string? Audio { get; set; }
+        public string? Wifi { get; set; }
+        public string? Usb { get; set; }
+        public string? Monitor { get; set; }
+        public string? Processador { get; set; }
+        public string? MotherBoard{ get; set; }
+
+        public List<string> SlotRaw { get; set; } = new List<string>();
+    }
+}
--- Builder/Builder/Domain/OperationalSystem.cs
+++ Builder/Builder/Domain/OperationalSystem.cs
@@ -2,5 +2,20 @@
 {
     public class OperationalSystem
     {
+        public string? Name { get; set; }
+        public int Raw { get; set; }
+        public string? Processador { get; set; }
+        public int Cpu { get; set; }
+        public string? Monitor { get; set; }
+        public string? Audio { get; set; }
+        public string? Video { get; set; }
+        public string? Wifi { get; set; }
+
+        public List<string> Programs { get; set; } = new List<string>();
+
+        public int ConsumerMemory()
+        {
+            return (this.Programs.Count / Raw) / Cpu;
+        }
     }
 }
