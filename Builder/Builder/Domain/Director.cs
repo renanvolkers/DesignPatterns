1450a789960647c811971b584c0c208bee22bfa9
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-14 11:37:43 -0300

    Add director responsabilite for control method

--- Builder/Builder/Domain/Director.cs
+++ Builder/Builder/Domain/Director.cs
@@ -2,11 +2,44 @@
 {
     public class Director
     {
-        public void ConstructProduct1(Builder builder)
+        public void OperationalSystemWindows(IBuilder builder)
         {
             builder.Reset();
-            builder.BuildStepA();
-            builder.BuildStepB();
+            builder.Name("Windows 10");
+            builder.Configuration();
+            builder.ConfigWireless();
+            builder.Processor(12);
+            builder.ConfigAudio();
+            builder.Monitor("34y");
+            builder.MotherBoard();
+            builder.Video();
+        }
+
+        public void OperationalSystemLinux(IBuilder builder)
+        {
+            builder.Reset();
+            builder.Name("Linux XX");
+            builder.Configuration();
+            builder.ConfigWireless();
+            builder.Processor(12);
+            builder.ConfigAudio();
+            builder.Monitor("34y");
+            builder.MotherBoard();
+            builder.Raw();
+            builder.USB();
+            builder.Video();
+        }
+
+        public void Buildcomputer(IBuilder builder)
+        {
+            builder.Reset();
+            builder.Name("Windows 10");
+            builder.Configuration();
+            builder.Raw();
+            builder.USB();
+            builder.Video();
+            builder.MotherBoard();
+           
         }
 
     }
