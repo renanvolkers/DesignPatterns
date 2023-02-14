8a10c6cc5a84969a32705dba5555ea15e78b788a
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-13 23:53:53 -0300

    Product can have some models

---
+++ Builder/Builder/Domain/OperationalSystemBuilder.cs
@@ -0,0 +1,57 @@
+﻿namespace Builder.Domain
+{
+    public class OperationalSystemBuilder : Builder
+    {
+       public OperationalSystem? OS { get; set; }
+
+        public OperationalSystemBuilder()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAudio()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetVideo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetWirelless()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetRaw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetUSB()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMonitor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetProcessor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMotherBoard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
