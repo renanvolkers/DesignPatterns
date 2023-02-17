6da69177d075faae3f3eafa18887adb666e31700
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-16 23:55:26 -0300

    Add EndPoint usign webapplication

---
+++ Prototype/Prototype/EndPoints/Points.cs
@@ -0,0 +1,26 @@
+﻿using AbstractFactory.Exemple_2.Domain;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbstractFactory.Example_1.EndPoints
+{
+    public static class ProductExemplo2Points
+    {
+        public static void MapProductExemplo2(this WebApplication app)
+        {
+            app.MapGet("/product/{name}", Get)
+                .ProducesValidationProblem()
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithName("getProduct")
+                .WithOpenApi();
+        }
+
+        public static async Task<IResult> Get(string name)
+        {
+            Product pro;
+            return pro is List<Product> ? Results.Ok(pro)
+                                  : Results.NotFound();
+        }
+
+    }
+}
