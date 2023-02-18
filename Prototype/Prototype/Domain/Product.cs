180c6a1c9b8d08c860dcee3020f6c38354df9450
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-17 21:22:25 -0300

    Add class product domain

---
+++ Prototype/Prototype/Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AbstractFactory.Exemple_2.Domain
+{
+    public class Product
+    {
+        [JsonIgnore]
+        public int Id       { get; set; }
+        public string? Name {get;set;}
+    }
+}
