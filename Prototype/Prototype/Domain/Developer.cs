c1bc1140c48ae44b696d856e536feba05933e09f
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-19 14:35:21 -0300

    Shallow Copy: only top-level objects are duplicated
    CloneDeep : Clone all deep not reference

--- Prototype/Prototype/Domain/Developer.cs
+++ Prototype/Prototype/Domain/Developer.cs
@@ -1,11 +1,29 @@
 ﻿namespace Prototype.Domain
 {
-    public class Developer
+    public class Developer : IEmployee
     {
         public int WordsPerMinute { get; set; }
         public string? Name { get; set; }
         public string? Role { get; set; }
         public string? PreferredLanguage { get; set; }
         public double MoneyPerHours { get; set; }
+
+        public IEmployee Clone()
+        {
+            return (IEmployee)MemberwiseClone();
+
+        }
+
+        public IEmployee CloneDeep()
+        {
+            return (IEmployee)this.Clone();
+        }
+
+        public string GetDetails()
+        {
+            return string.Format("{0} - {1} - {2}", Name, Role, PreferredLanguage);
+        }
     }
+
+
 }
