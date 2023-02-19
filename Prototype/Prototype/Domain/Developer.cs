f8c39b73b830e149d3b460ad3ada79961edcc23c
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-19 14:30:25 -0300

    Add 'ConcretePrototype' class

--- Prototype/Prototype/Domain/Developer.cs
+++ Prototype/Prototype/Domain/Developer.cs
@@ -2,5 +2,10 @@
 {
     public class Developer
     {
+        public int WordsPerMinute { get; set; }
+        public string? Name { get; set; }
+        public string? Role { get; set; }
+        public string? PreferredLanguage { get; set; }
+        public double MoneyPerHours { get; set; }
     }
 }
