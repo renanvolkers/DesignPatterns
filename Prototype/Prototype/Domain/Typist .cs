dfb3cc7cc74c7f5ff91ca5f2b44191f563e6b083
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-19 19:57:49 -0300

    Add concrete class prototype

---
+++ Prototype/Prototype/Domain/Typist .cs
@@ -0,0 +1,29 @@
+﻿using Prototype.Domain;
+
+namespace Prototype.Domain
+{
+    public class Typist : IStaff
+    {
+        public int WordsPerMinute { get; set; }
+        public string? Name { get; set; }
+        public string? Role { get; set; }
+        public List<Experience> Experiences { get; set; } = new List<Experience>();
+
+
+        public IStaff ShallowClone()
+        {
+            return (IStaff)MemberwiseClone();
+
+        }
+
+        public IStaff DeepClone()
+        {
+            return (IStaff)this.ShallowClone();
+        }
+
+        public string GetDetails()
+        {
+            return string.Format("{0} - {1} - {2}", Name, Role, WordsPerMinute);
+        }
+    }
+}
