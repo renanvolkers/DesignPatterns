b80a6bbac99e0f449e0c03d09365f5016ed687dd
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-23 10:09:05 -0300

    Add propriedty interface

--- Prototype/Prototype/Domain/IStaff.cs
+++ Prototype/Prototype/Domain/IStaff.cs
@@ -1,8 +1,12 @@
-﻿namespace Prototype.Domain
+﻿using Microsoft.VisualBasic;
+
+namespace Prototype.Domain
 {
     public interface IStaff
     {
         public Guid Id { get; set; }
+        public List<Experience> Experiences { get; set; }
+        public InfoStaff InfoStaff { get; set; }
         string? Name { get; set; }
         IStaff ShallowClone();
         IStaff DeepClone();
