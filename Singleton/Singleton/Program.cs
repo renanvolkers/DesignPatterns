e52f1c3c5735b0efa506c2817579f272a742c9e6
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-23 18:23:18 -0300

    Start Project Singleton

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers().AddJsonOptions(options =>
+           options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+
+builder.Services.AddSwaggerGen(op =>
+{
+    op.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Delivery Pizza",
+        Version = "version",
+        Description = "The Goal project is show the use pattern Abstract Factory",
+        Contact = new OpenApiContact
+        {
+            Name = "Renan Alpoim Volkers",
+            Email = "renanvolkers@hotmail.com",
+            Url = new Uri("https://www.linkedin.com/in/renanvolkers/"),
+
+        }
+    });
+
+});
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+
