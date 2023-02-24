338fc8909c8bed2370ec6a7876723ce917817f51
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-24 08:11:35 -0300

    MapEndPoints Method Get Http.

---
+++ Singleton/Singleton/EndPoint/EndPoints.cs
@@ -0,0 +1,28 @@
+﻿using Singleton.Domain;
+
+namespace Singleton.EndPoints
+{
+    public static class  EndPoints
+    {
+        public static List<int> contagem { get; set; } = new List<int>();
+        public static void MapEndPoints(this WebApplication app)
+        {
+            app.MapGet("/Singleton/", Get)
+                .ProducesValidationProblem()
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithName("Singleton")
+                .WithOpenApi();
+
+        }
+
+        public static  IResult Get()
+        {
+            var teste = new Application();
+            var result = teste.VerificarVetor();
+            return result is VetorExercice ? Results.Ok(result)
+                                  : Results.NotFound();
+        }
+
+
+    }
+}
