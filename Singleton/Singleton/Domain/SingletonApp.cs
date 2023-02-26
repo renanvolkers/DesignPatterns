8a0f39384fa8483b8bf19b9a07f85817d6e5385f
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-25 23:34:24 -0300

    The singleton's constructor should always be private to prevent direct construction calls with the `new`  operator.

--- Singleton/Singleton/Domain/SingletonApp.cs
+++ Singleton/Singleton/Domain/SingletonApp.cs
@@ -2,5 +2,15 @@
 {
     public class SingletonApp
     {
+        #region Singleton Design Pattern 
+
+        private static SingletonApp _mySingleton;
+
+        private SingletonApp()
+        {
+            TestSingleton = new List<string>();
+        }
+        public List<string> TestSingleton { get; set; }
+        #endregion
     }
 }
