b4b8e4912f92aef21054182b63160f0c338526a0
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-25 23:41:06 -0300

    The static method that controls access to the singleton instance.

--- Singleton/Singleton/Domain/SingletonApp.cs
+++ Singleton/Singleton/Domain/SingletonApp.cs
@@ -10,6 +10,13 @@
         {
             TestSingleton = new List<string>();
         }
+        public static SingletonApp GetMySingleton()
+        {
+            if (_mySingleton == null)
+                _mySingleton = new SingletonApp();
+
+            return _mySingleton;
+        }
         public List<string> TestSingleton { get; set; }
         #endregion
     }
