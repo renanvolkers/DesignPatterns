f44eb904748e8ef6bade285cfb23a2f63681bca2
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-27 11:23:23 -0300

    Add GetSingletoVetor test some instance

--- Singleton/Singleton/EndPoint/SingletonAppEndPoints.cs
+++ Singleton/Singleton/EndPoint/SingletonAppEndPoints.cs
@@ -2,7 +2,7 @@
 
 namespace Singleton.EndPoints
 {
-    public static class  SingletonAppEndPoints
+    public static class SingletonAppEndPoints
     {
         public static List<int> contagem { get; set; } = new List<int>();
         public static void MapSingletonAppEndPoints(this WebApplication app)
@@ -13,13 +13,27 @@ namespace Singleton.EndPoints
                 .WithName("Singleton")
                 .WithOpenApi();
 
+            app.MapGet("/SameInstance/", GetSingletoVetor)
+                .ProducesValidationProblem()
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithName("SameInstance")
+                .WithOpenApi();
+
         }
 
-        public static  IResult Get()
+        public static IResult Get()
         {
             var teste = new Application();
             var result = teste.VerificarVetor();
-            return result is VetorExercice ? Results.Ok(result)
+            return result is VetorExercice<int> ? Results.Ok(result)
+                                  : Results.NotFound();
+        }
+
+        public static IResult GetSingletoVetor()
+        {
+            var teste = new Application();
+            var result = teste.SameInstance();
+            return result is VetorExercice<string> ? Results.Ok(result)
                                   : Results.NotFound();
         }
 
