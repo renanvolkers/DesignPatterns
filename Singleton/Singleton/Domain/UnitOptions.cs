64198e595684c4319b2ccd3d4ea33d4a8eb03bd3
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-28 23:12:27 -0300

    Class for Serialize object in appsetting

---
+++ Singleton/Singleton/Domain/UnitOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Singleton.Domain
+{
+    public class UnitOptions
+    {
+        public string Temp { get; set; } = String.Empty;
+        public string Distance { get; set; } = String.Empty;
+    }
+}
