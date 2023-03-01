bcfea090da240f3dc0ca86dc0b1dc3216517303b
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-02-28 23:10:59 -0300

    Class Service Singleton find appconfig

---
+++ Singleton/Singleton/Domain/SingletonService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Singleton.Domain
+{
+    public class SingletonService : ISingletonService
+    {
+        private readonly IOptionsMonitor<UnitOptions> _unitOptions;
+
+        public SingletonService(IOptionsMonitor<UnitOptions> unitOptions)
+        {
+            _unitOptions = unitOptions;
+        }
+        public UnitOptions GetUnits()
+        {
+            return _unitOptions.CurrentValue;
+        }
+    }
+}
