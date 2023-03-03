fb463d7e67bce8dbe27ffcb7a3b1d9d30b651cd4
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-02 23:39:21 -0300

    Add classe command Oracle method database

---
+++ Adapter/Adapter/Domain/OracleCommands.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Adapter.Domain
+{
+    public class OracleCommands : DbAdapter
+    {
+        public void Delete()
+        {
+            throw new NotImplementedException("Delete Oracle");
+        }
+
+        public void Insert()
+        {
+            throw new NotImplementedException("Insert Oracle");
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException("Update Oracle");
+        }
+    }
+}
