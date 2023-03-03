469daadf1815efe156dfa6d3ed1b5216e69c5cc1
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-02 22:58:22 -0300

    Add endpoint Get consult

---
+++ Adapter/Adapter/EndPoint/AdapterEndPoints.cs
@@ -0,0 +1,27 @@
+﻿namespace Adapter.EndPoints
+{
+    public static class AdapterEndPoints
+    {
+        public static List<int> contagem { get; set; } = new List<int>();
+        public static void MapSingletonAppEndPoints(this WebApplication app)
+        {
+            app.MapGet("/Singleton/", Get)
+                .ProducesValidationProblem()
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithName("Singleton")
+                .WithOpenApi();
+
+        }
+
+        public static IResult Get()
+        {
+            //var teste = new Application();
+            //var result = teste.VerificarVetor();
+            //return result is VetorExercice<int> ? Results.Ok(result)
+            //                      : Results.NotFound();
+            return null;
+        }
+
+
+    }
+}
