ec101f04db325a7dfa533b188b51273f284c7707
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-02 23:41:02 -0300

    Add Commands Mysql database

---
+++ Adapter/Adapter/Domain/MySqlCommands.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Adapter.Domain
+{
+    public class MySqlCommands 
+    {
+        public void DeleteMySql()
+        {
+            throw new NotImplementedException("Delete MySql");
+        }
+
+        public void InsertMySql()
+        {
+            throw new NotImplementedException("Insert MySql");
+        }
+
+        public void UpdateMySql()
+        {
+            throw new NotImplementedException("Update MySql");
+        }
+    }
+}
