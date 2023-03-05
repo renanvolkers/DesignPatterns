3f5183384466f5a78d8a2e0c6edb3c4b52cf3054
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-04 23:18:00 -0300

    Add construct parameter Myssqlcommands

--- Adapter/Adapter/Domain/MySqlAdapter.cs
+++ Adapter/Adapter/Domain/MySqlAdapter.cs
@@ -1,6 +1,25 @@
 ﻿namespace Adapter.Domain
 {
-    public class MySqlAdapter
+    public class MySqlAdapter : DbAdapter
     {
+        private MySqlCommands SqlCommand { get; set; }
+         public MySqlAdapter(MySqlCommands _sqlCommand)
+        {
+            this.SqlCommand = _sqlCommand;
+        }
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
