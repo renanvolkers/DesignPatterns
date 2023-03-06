7b7945e01f83c1da30f719b358293d23f65e40ca
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-05 23:53:11 -0300

    Add Configuration Swagger title and Json form query

--- Adapter/Adapter/Program.cs
+++ Adapter/Adapter/Program.cs
@@ -1,10 +1,31 @@
+using Adapter.EndPoints;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers().AddJsonOptions(options =>
+           options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(op =>
+{
+    op.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Adapter",
+        Version = "version",
+        Description = "The Goal project is show the use pattern Adapter",
+        Contact = new OpenApiContact
+        {
+            Name = "Renan Alpoim Volkers",
+            Email = "renanvolkers@hotmail.com",
+            Url = new Uri("https://www.linkedin.com/in/renanvolkers/"),
 
+        }
+    });
+
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,7 +37,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.MapAdapterAppEndPoints();
 app.Run();
 
 
