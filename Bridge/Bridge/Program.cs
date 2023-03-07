4d63b5266c25cf3aa59f7dd7038603b5a954e000
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-06 22:59:04 -0300

    Start configuration design project model

---
+++ Bridge/Bridge/Program.cs
@@ -0,0 +1,53 @@
+
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+namespace Bridge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers().AddJsonOptions(options =>
+           options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(op =>
+            {
+                op.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Bridge",
+                    Version = "version",
+                    Description = "The Goal project is show the use pattern Bridge",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Renan Alpoim Volkers",
+                        Email = "renanvolkers@hotmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/renanvolkers/"),
+
+                    }
+                });
+
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.Run();
+        }
+    }
+}
---
+++ Bridge/README.md
@@ -0,0 +1,28 @@
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+[![GitHub version](https://badge.fury.io/gh/Naereen%2FStrapDown.js.svg)](https://github.com/Naereen/StrapDown.js)
+
+
+
+![Coverage](https://github.com/renanvolkers/DesignPatterns/main/coverage_badge.svg?sanitize=true)
+
+# Main Stacks
+* .NET 7 
+* Minimal API
+
+
+
+# Bridge
+Is a structural design pattern that lets you split a large class or a set of closely related classes into two separate hierarchies—abstraction and implementation—which can be developed independently of each other.
+
+
+# Register Staff 
+
+
+
+Configuration Program EntityFramework
+//builder.Services.AddIdentityEntityFrameworkContextConfiguration(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+//    b => b.MigrationsAssembly("MinimalPilot")));
+
+
+
