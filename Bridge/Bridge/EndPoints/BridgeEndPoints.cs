4ffe700943b820fa672036494e260739abf1febc
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-08 05:31:20 -0300

    Add Method call EndPoint with VERB http Post,Get

---
+++ Bridge/Bridge/EndPoints/BridgeEndPoints.cs
@@ -0,0 +1,30 @@
+﻿using Bridge.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bridge.EndPoints
+{
+    public static class BridgeEndPoints
+    {
+        public static void MapBridgeAppEndPoints(this WebApplication app)
+        {
+            app.MapGet("/Bridge/", Post)
+                .ProducesValidationProblem()
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithName("Bridge")
+                .WithOpenApi();
+
+        }
+
+        public static IResult Post([FromQuery]TypeFile type,Employee employee)
+        {
+            Application app = new Application();
+            app.FileEmployee(employee, type);
+ 
+
+            return employee is Employee ? Results.Ok(employee)
+                                  : Results.NotFound();
+        }
+
+
+    }
+}
