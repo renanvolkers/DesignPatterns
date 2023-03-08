45061bd105a5e886d3d796c0de55bfc9475bee19
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-07 23:08:37 -0300

    This class represents another ConcreteImplementor class and writes the given Employee details to     /// a JSON file

---
+++ Bridge/Bridge/Domain/JSONWriter .cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace Bridge.Domain
+{
+    /// <summary>
+    /// This class represents another ConcreteImplementor class and writes the given Employee details to 
+    /// a JSON file.
+    /// </summary>
+    public class JSONWriter : IFileWriter
+    {
+        string fileName = "EmployeeSalaryDetails.json";
+
+        public void WriteFile(Employee employee)
+        {
+            var serializedEmployeeData = JsonSerializer.Serialize(employee);
+            File.WriteAllText(fileName, serializedEmployeeData);
+            Console.WriteLine($"EmployeeId# : {employee.Id} salary details sucessfully written to {fileName} ");
+        }
+    }    {
+    }
+}
