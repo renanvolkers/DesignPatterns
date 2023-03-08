5c1ab3a67dfcfbdc25f58dfbc4aadbe40549e6da
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-08 05:33:50 -0300

    Represente class with method Core for execute.

--- Bridge/Bridge/Domain/Application.cs
+++ Bridge/Bridge/Domain/Application.cs
@@ -2,5 +2,22 @@
 {
     public class Application
     {
+        public void FileEmployee(Employee employee,TypeFile file)
+        {
+            SalaryCalculator salaryCalculator;
+            if (TypeFile.Xml == file)
+            {
+                salaryCalculator = new SalaryCalculator(new XMLWriter());
+                salaryCalculator.ProcessEmployeeSalary(employee);
+                employee.Incentive = 2500;
+            }
+            else
+            {
+                salaryCalculator = new SalaryCalculator(new JSONWriter());
+                salaryCalculator.ProcessEmployeeSalary(employee);
+            }
+
+        }
+
     }
 }
