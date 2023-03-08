0d1eefd2b6858988a21f49b93f052502011420d2
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-07 23:03:45 -0300

    This class represent an Employee

---
+++ Bridge/Bridge/Domain/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Bridge.Domain
+{
+    /// <summary>
+    /// This class represent an Employee
+    /// </summary>
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public decimal Incentive { get; set; }
+        public decimal TotalSalary { get; set; }
+    }
+}
