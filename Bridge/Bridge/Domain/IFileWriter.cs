cb17f1e8f39969fbceec20e84ca5281c08db5b00
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-07 23:05:02 -0300

    The interface represents the Implementor interface from the design pattern and exposes a single method     /// to write a given employee details to a file

---
+++ Bridge/Bridge/Domain/IFileWriter.cs
@@ -0,0 +1,11 @@
+﻿namespace Bridge.Domain
+{
+    /// <summary>
+    /// The interface represents the Implementor interface from the design pattern and exposes a single method 
+    /// to write a given employee details to a file
+    /// </summary>
+    public interface IFileWriter
+    {
+        void WriteFile(Employee employee);
+    }
+}
