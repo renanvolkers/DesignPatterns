e51f9c1510c93a59a46cfabf916acb56f8e864e2
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-07 23:12:18 -0300

    This abstract class represents the Abstraction from the pattern and have different method     /// than that of Implementor interface.

---
+++ Bridge/Bridge/Domain/ContentWriter.cs
@@ -0,0 +1,21 @@
+﻿namespace Bridge.Domain
+{
+    /// <summary>
+    /// This abstract class represents the Abstraction from the pattern and have different method 
+    /// than that of Implementor interface.
+    /// </summary>
+    public abstract class ContentWriter
+    {
+        protected IFileWriter fileWriter;
+
+        public ContentWriter(IFileWriter fileWriter)
+        {
+            this.fileWriter = fileWriter;
+        }
+
+        public virtual void WriteContent(Employee employee)
+        {
+            fileWriter.WriteFile(employee);
+        }
+    }
+}
