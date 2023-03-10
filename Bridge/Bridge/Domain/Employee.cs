ec781a5c4039f1d21d7ac36a4bce6cf9cb12925f
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-09 21:00:23 -0300

    Include Construct

--- Bridge/Bridge/Domain/Employee.cs
+++ Bridge/Bridge/Domain/Employee.cs
@@ -5,6 +5,7 @@
     /// </summary>
     public class Employee
     {
+        public Employee() { }
         public int Id { get; set; }
         public string? Name { get; set; }
         public decimal Basic { get; set; }
