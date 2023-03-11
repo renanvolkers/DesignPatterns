ecad29ed593c83763aa83dc868b1d93445ba7ac5
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-10 21:43:54 -0300

    Include configuration Swagger.

--- Composite/Composite/Program.cs
+++ Composite/Composite/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.OpenApi.Models;
+
 namespace Composite
 {
     public class Program
@@ -12,8 +14,24 @@ namespace Composite
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
 
+            builder.Services.AddSwaggerGen(op =>
+            {
+                op.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "",
+                    Version = "version",
+                    Description = "The Goal project is show the use pattern Builder",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Renan Alpoim Volkers",
+                        Email = "renanvolkers@hotmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/renanvolkers/"),
+
+                    }
+                });
+
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
