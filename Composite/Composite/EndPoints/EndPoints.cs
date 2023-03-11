9d3859c414c203b992ab01cbef047d85e5baea96
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-10 21:47:32 -0300

    Class http with Post,Get

---
+++ Composite/Composite/EndPoints/EndPoints.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AbstractFactory.EndPoints
+{
+    public static class  EndPoints
+    {
+        public static void MapComputerEndPoints(this WebApplication app)
+        {
+
+            app.MapPost("/createOperationalSystem/", CreateOs)
+                .ProducesValidationProblem()
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithName("createOperationalSystem")
+                .WithOpenApi();
+
+        }
+
+        public static  IResult CreateOs()
+        {
+
+            return ""  is string ? Results.Created("teste", "")
+                                  : Results.BadRequest();
+        }
+
+
+    }
+}
