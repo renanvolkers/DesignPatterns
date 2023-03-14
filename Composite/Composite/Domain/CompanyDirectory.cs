050453bee669a295e1b9c78cbf06db8dd2124a88
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-13 21:40:22 -0300

    Add class companyDirectory

---
+++ Composite/Composite/Domain/CompanyDirectory.cs
@@ -0,0 +1,32 @@
+﻿namespace Composite.Domain
+{
+    public class CompanyDirectory : Employee
+    {
+
+        private List<Employee> EmployeeList { get; set; } = new List<Employee>();
+
+
+        public string ShowEmployeeDetails()
+        {
+            var msg = " ";
+            foreach (var emp in EmployeeList)
+            {
+                msg += emp.ShowEmployeeDetails();
+            }
+            return msg;
+
+        }
+    }
+
+    public void AddEmployee(Employee emp)
+    {
+        this.EmployeeList.Add(emp);
+    }
+
+    public void RemoveEmployee(Employee emp)
+    {
+        EmployeeList.Remove(emp);
+    }
+}
+
+
