b6388645ba0b0cf88fc5ecf1638c678fdf0469fa
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-14 06:19:52 -0300

    Add propriedly Name and EmpId Add Method ShowEmployeeDetails

--- Composite/Composite/Domain/Manager.cs
+++ Composite/Composite/Domain/Manager.cs
@@ -1,6 +1,14 @@
-﻿namespace Composite.Domain
+﻿using System.Xml.Linq;
+
+namespace Composite.Domain
 {
     public class Manager : Employee
     {
+        private String name;
+        private long empId;
+        public string ShowEmployeeDetails()
+        {
+            return empId + " " + name;
+        }
     }
 }
