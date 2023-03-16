f70bf93cb4890277f9f6afc1fed5a3a2f611c25a
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-16 06:05:01 -0300

    Include new Construct Method show message the position.

--- Composite/Composite/Domain/Manager.cs
+++ Composite/Composite/Domain/Manager.cs
@@ -6,11 +6,20 @@ namespace Composite.Domain
 
     public class Manager : Employee
     {
+
+        public Manager(int empId,string name, string position)
+        {
+            this.Name = name;
+            this.EmpId = EmpId;
+            this.Position = position;
+        }
+
         private string? Name { get; set; }
         private long EmpId { get; set; }
+        private string? Position { get; set; }
         public string ShowEmployeeDetails()
         {
-            return EmpId + " " + Name;
+            return "/n "+EmpId + " " + Name + " " + Position;
         }
     }
 }
