1abd6912f1191585c1184a7b7f701b6af2c1acf2
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-17 05:58:47 -0300

    EndPoint Company result hierarchy.

---
+++ Composite/Composite/EndPoints/CompanyEndPoints.cs
@@ -0,0 +1,29 @@
+﻿using Composite.Domain;
+
+namespace Composite.EndPoints
+{
+    public static class CompanyEndPoints
+    {
+        public static void MapCompanyEndPoints(this WebApplication app)
+        {
+
+            app.MapPost("/Company/", Get)
+                .ProducesValidationProblem()
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithName("Company")
+                .WithOpenApi();
+
+        }
+
+        public static  IResult Get()
+        {
+            Company company = new Company();
+            var hierarchy = company.Execurte();
+
+            return hierarchy is string ? Results.Ok(hierarchy)
+                                  : Results.BadRequest();
+        }
+
+
+    }
+}
