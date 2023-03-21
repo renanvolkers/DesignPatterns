1f06e369c81afc948f31d08eb9534a04feb5d349
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-20 23:59:16 -0300

    Configuration program swagger

--- Decorator/Decorator/Program.cs
+++ Decorator/Decorator/Program.cs
@@ -6,13 +6,31 @@ namespace Decorator
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers().AddJsonOptions(options =>
+           options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
             // Add services to the container.
             builder.Services.AddAuthorization();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(op =>
+            {
+                op.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Bridge",
+                    Version = "version",
+                    Description = "The Goal project is show the use pattern Bridge",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Renan Alpoim Volkers",
+                        Email = "renanvolkers@hotmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/renanvolkers/"),
+
+                    }
+                });
+
+            });
 
             var app = builder.Build();
 
