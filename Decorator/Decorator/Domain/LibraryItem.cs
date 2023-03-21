ffe021dc70051a19109b3270a2a70ca5509b0538
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-21 06:39:50 -0300

    The 'Component' abstract class implement contructos, methods

--- Decorator/Decorator/Domain/LibraryItem.cs
+++ Decorator/Decorator/Domain/LibraryItem.cs
@@ -1,6 +1,15 @@
 ﻿namespace Decorator.Domain
 {
+    /// <summary>
+    /// The 'Component' abstract class
+    /// </summary>
     public class LibraryItem
     {
+        private int _numCopies;
+        public int NumCopies
+        {
+            get { return _numCopies; }
+            set { _numCopies = value; }
+        }
     }
 }
