e5855e8e6397b293b2edb025a65e0ac38fb65984
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-23 20:12:53 -0300

    Propriedly List string

--- Decorator/Decorator/Domain/Decorator .cs
+++ Decorator/Decorator/Domain/Decorator .cs
@@ -1,6 +1,10 @@
 ﻿namespace Decorator.Domain
 {
+    /// <summary>
+    /// The 'ConcreteDecorator' class
+    /// </summary>
     public class Decorator
     {
+        protected readonly List<string> Borrowers = new List<string>();
     }
 }
