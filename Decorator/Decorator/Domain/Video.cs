b4b4d9fcb46c315a0725cc52bcb7a8dac1262819
Renan Alpoim Volkers <renanvolkers@hotmail.com>
2023-03-23 20:08:55 -0300

    Implement interface LibraryItem Display

--- Decorator/Decorator/Domain/Video.cs
+++ Decorator/Decorator/Domain/Video.cs
@@ -1,6 +1,8 @@
-﻿namespace Decorator.Domain
+﻿using System.IO;
+
+namespace Decorator.Domain
 {
-    public class Video
+    public class Video : LibraryItem
     {
         private string? Diretor { get; set; }
         private string? Title { get; set; }
@@ -13,5 +15,15 @@
             PlayTime = playTime;
         }
 
+        public override string Display()
+        {
+            var msg ="\nVideo ----- ";
+            string.Concat(msg, " Director:", this.Diretor);
+            string.Concat(msg, " Director:", this.Title);
+            string.Concat(msg, " Director:", this.NumCopies);
+            string.Concat(msg, " Director:", this.PlayTime);
+
+            return msg;
+        }
     }
 }
